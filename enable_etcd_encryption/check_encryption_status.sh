b6fd0a38ac230155ed786e6cb5277cf507e3f277
Thomas Jungbauer <tjungbau@redhat.com>
2022-07-06 18:52:12 +0200

    cleanup

--- enable_etcd_encryption/check_encryption_status.sh
+++
@@ -1,5 +0,0 @@
-oc get openshiftapiserver -o=jsonpath='{range .items[0].status.conditions[?(@.type=="Encrypted")]}{.reason}{"\n"}{.message}{"\n"}'
-
-echo ""
-
-oc get kubeapiserver -o=jsonpath='{range .items[0].status.conditions[?(@.type=="Encrypted")]}{.reason}{"\n"}{.message}{"\n"}'
